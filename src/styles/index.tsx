f86c0dfdb48d48235a1148ab2f348e588a572fc8
koofrank <koofranker@gmail.com>
2019-10-17 15:56:36 +0800

    initp project

---
+++ src/styles/index.tsx
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  // You can continue writing global styles here
+  body {
+    padding: 0;
+    background-color: black;
+  }
+`;
